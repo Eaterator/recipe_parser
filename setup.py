2fe68cf39511312951e484b259014dfaaffb4085
lucas <currahl@yahoo.ca>
2017-03-06 13:22:57 -0500

    Setup.py name change

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='recipe-parser',
     version='0.1',
     description="Parser for ingredients and quantities in a recipe using NLTK and heuristics",
+    url="https://github.com/Eaterator/recipe_parser",
     author="Lucas Currah",
     license="MIT",
     keywords="recipe ingredient quantity cooking parser",
