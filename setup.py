e616d94c6ef3198e3023c4a8e0c0eebdcd3ac229
lucas <currahl@yahoo.ca>
2017-03-06 13:14:57 -0500

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='Recipe Parser',
-    version='0.1'
+    version='0.1',
+    description="Parser for ingredients and quantities in a recipe using NLTK and heuristics",
+    author="Lucas Currah",
+    license="MIT",
+    keywords="recipe ingredient quantity cooking parser",
+    packages=find_packages(exclude=['tests*', 'docs'])
 )
